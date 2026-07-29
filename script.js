1a3e4a619f7a9d9605ddad9e1a8645f4c56142f1
Nadia Rocha <nadiroch@contactcenter.com>
2026-07-29 15:39:32 -0300

    Galería generada desde JavaScript

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("Pintá con Bauti iniciado");
+const dibujos = [
+
+    {
+        nombre: "Bartolito",
+        archivo: "huevo_bartolito.png"
+    }
+
+];
+
+const galeria = document.getElementById("galeria");
+
+dibujos.forEach(dibujo => {
+
+    galeria.innerHTML += `
+
+        <div class="tarjeta">
+
+            <img src="dibujos/${dibujo.archivo}" alt="${dibujo.nombre}">
+
+            <p>${dibujo.nombre}</p>
+
+        </div>
+
+    `;
+
+});
